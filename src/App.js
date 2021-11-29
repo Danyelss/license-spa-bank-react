ded31cff659f275895a9b236e51595972e4d8ae7
Danyelss <mos.dani@yahoo.com>
2021-11-29 23:35:01 +0200

    done deploying

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
-import {
-    getAccessToken,
-    //getRefreshToken
+import { getAccessToken, 
+  //getRefreshToken
 } from './token/Tokens';
 
 import LoginPage from './pages/LoginPage';
@@ -9,47 +8,39 @@ import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
 
 import {
-    Routes,
-    Route,
-    BrowserRouter,
-    Navigate,
+  Routes,
+  Route,
+  BrowserRouter,
+  Navigate,
 } from "react-router-dom";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        BrowserRouter >
-        <
-        Routes > { /*<Route path="/" element={<HomePage />} />*/ } <
-        Route exact path = "/login"
-        element = { < LoginPage / > }
-        /> <
-        Route exact path = "/404"
-        element = { < ErrorPage / > }
-        /> <
-        Route path = "*"
-        element = { < Navigate replace to = "/404" / > }
-        /> <
-        Route path = "/home"
-        element = { <
-            PrivateRoute >
-            <
-            HomePage / >
-            <
-            /PrivateRoute>
-        }
-        /> <
-        /Routes> <
-        /BrowserRouter> <
-        /div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/404" element={<ErrorPage />} />
+          {/*<Route path="*" element={<Navigate replace to="/404" />} />*/}
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 function PrivateRoute({ children }) {
-    const auth = getAccessToken();
+  const auth = getAccessToken();
 
-    return auth ? children : < Navigate to = "/login" / > ;
+  return auth ? children : <Navigate to="/login" />;
 }
 
 export default App;
